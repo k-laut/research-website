e9851452fff3cf384fb49b3e25f50811b72b082d
Kateryna Smykovska <72748322+k-smyk@users.noreply.github.com>
2025-12-30 14:21:35 +0100

    created quick skiplinks components as parent

---
+++ src/components/accessibility/SkipLinks.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+export function SkipLinks() {
+  return (
+    <div className="skip-links">
+      <a href="#main-content" className="skip-link">
+        Skip to main content
+      </a>
+      <a href="#kramer-research" className="skip-link">
+        Skip to Krämer Research
+      </a>
+      <a href="#lauterbach-research" className="skip-link">
+        Skip to Lauterbach Research
+      </a>
+    </div>
+  );
+}
